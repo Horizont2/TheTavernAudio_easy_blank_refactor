29834d66ae77b7a0181b655154f23347b85b24dc
Bohdan Honcharuk <bhoncharuk@edu.cdv.pl>
2026-05-08 12:59:11 +0200

    Signed-off-by: Bohdan Honcharuk <bhoncharuk@edu.cdv.pl>

--- Assets/Scripts/VCA.cs
+++ Assets/Scripts/VCA.cs
@@ -1,43 +1,34 @@
 using UnityEngine;
 using FMODUnity;
-using FMOD.Studio;
 
 /// <summary>
 /// Zarządza głośnością ścieżek audio poprzez FMOD VCAs.
 /// </summary>
 public class VCA : MonoBehaviour
 {
-    // FMOD - Referencje do VCAs.
     private FMOD.Studio.VCA globalVCA;
     private FMOD.Studio.VCA musicVCA;
     private FMOD.Studio.VCA tavernVCA;
     private FMOD.Studio.VCA outsideVCA;
 
-    // Flagi stanu wyciszenia.
-    [SerializeField]
-    private bool globalMuteActive = true;
-    [SerializeField]
-    private bool musicMuteActive = false;
-    [SerializeField]
-    private bool tavernMuteActive = false;
-    [SerializeField]
-    private bool outsideMuteActive = false;
+    [SerializeField] private bool globalMuteActive = false;
+    [SerializeField] private bool musicMuteActive = false;
+    [SerializeField] private bool tavernMuteActive = false;
+    [SerializeField] private bool outsideMuteActive = false;
 
     void Start()
     {
-        // Pobiera VCAs z FMOD.
-        globalVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Mute");
-        musicVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Music");
-        tavernVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Tavern_amb");
-        outsideVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Outside_amb");
+        globalVCA = RuntimeManager.GetVCA("vca:/Mute");
+        musicVCA = RuntimeManager.GetVCA("vca:/Music");
+        tavernVCA = RuntimeManager.GetVCA("vca:/Tavern_amb");
+        outsideVCA = RuntimeManager.GetVCA("vca:/Outside_amb");
 
-        // Ustawia początkową głośność.
-        globalVCA.setVolume(DecibelToLinear(-100));
+        // globalMuteActive = true;
+        // globalVCA.setVolume(0.0f); 
     }
 
     void Update()
     {
-        // Sprawdza, czy klawisze zostały naciśnięte i wywołuje odpowiednią funkcję.
         if (Input.GetKeyDown(KeyCode.U))
         {
             ToggleMute(globalVCA, ref globalMuteActive);
@@ -56,23 +47,12 @@ public class VCA : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Przełącza głośność VCA na 0 dB (włączony) lub -100 dB (wyciszony).
-    /// </summary>
-    /// <param name="vca">VCA do przełączenia.</param>
-    /// <param name="muteFlag">Zmienna stanu, która jest przełączana.</param>
     private void ToggleMute(FMOD.Studio.VCA vca, ref bool muteFlag)
     {
-        muteFlag = !muteFlag; // Odwraca stan wyciszenia.
-        float volume = muteFlag ? DecibelToLinear(-100) : DecibelToLinear(0);
-        vca.setVolume(volume);
-    }
+        muteFlag = !muteFlag;
 
-    /// <summary>
-    /// Konwertuje decybele na wartość liniową.
-    /// </summary>
-    private float DecibelToLinear(float dB)
-    {
-        return Mathf.Pow(10.0f, dB / 20f);
+        float volume = muteFlag ? 0.0f : 1.0f;
+
+        vca.setVolume(volume);
     }
 }
